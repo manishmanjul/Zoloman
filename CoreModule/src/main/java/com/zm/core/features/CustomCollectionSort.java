f27c8a84ae470c150655664d3d58b5a4102c9949
Manish.Manjul <Manish.Manjul@INLT1512.sapiens.int>
2019-09-17 16:41:21 +0530

    lamda and collection

---
+++ CoreModule/src/main/java/com/zm/core/features/CustomCollectionSort.java
@@ -0,0 +1,14 @@
+package com.zm.core.features;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class CustomCollectionSort <T,R> {
+
+	ArrayList<T> myList ;
+	HashMap<T, R> myMap;
+	HashSet<T> mySet;
+
+}
